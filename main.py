4657ccdd120d057bc6131bd051e24fdbff911375
Oliver Tuesta <oliver.jtuesta@gmail.com>
2024-02-12 14:45:03 -0500

    feat: add get all companies from worldbank

--- main.py
+++ main.py
@@ -50,3 +50,49 @@ async def search_company(company_name: str):
         raise HTTPException(status_code=404, detail="No results found")
 
     return {"results": results}
+
+@app.get("/debarred-firms")
+async def get_debarred_firms():
+    url = "https://projects.worldbank.org/en/projects-operations/procurement/debarred-firms"
+    try:
+        # Use Selenium to load JavaScript content
+        service = Service(chrome_path)
+        driver = webdriver.Chrome()
+        driver.get(url)
+        time.sleep(2)  # Adjust the delay time as necessary to ensure the page is fully loaded
+        page_source = driver.page_source
+        driver.quit()
+
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Error fetching data: {str(e)}")
+    
+    soup = BeautifulSoup(page_source, 'html.parser')
+    results = []
+
+    div = soup.find('div', class_='k-grid-content k-auto-scrollable')
+    table = div.find('table')
+    if table:
+        print("Table found")
+        rows = table.find_all('tr')
+        for row in rows[1:]:  # Skip header row
+            columns = row.find_all('td')
+            print(columns)
+            if len(columns) > 0:  # Ensure all columns are present
+                firm_name = columns[0].text.strip()
+                address = columns[1].text.strip()
+                #country = columns[3].text.strip()
+                #ineligibility_from = columns[4].text.strip()
+                #ineligibility_to = columns[5].text.strip()
+                #grounds = columns[6].text.strip()
+                results.append({
+                    "firm_name": firm_name,
+                    "address": address,
+                 #   "country": country,
+                  #  "ineligibility_period": {
+                   #     "from_date": ineligibility_from,
+                    #    "to_date": ineligibility_to
+                    #},
+                    #"grounds": grounds
+                })
+
+    return {"results": results}
